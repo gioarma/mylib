0b96d775874ac67ccb2837cab4ed980afcae5fd9
Giovanni Armaroli <giovanni.armaroli@gmail.com>
2022-02-14 16:15:27 +0100

    add back utils file

---
+++ utils.py
@@ -0,0 +1,11 @@
+def filter_df (df, filter_dict):
+    '''
+    Filters a DataFrame keeping only the rows that contain the values indicated in the filter
+    df: DataFrame to be filtered. \n
+    filter_dict: dictionary where the keys are column names of the DataFrame and the values are lists of values to be searched in the columns. Only the rows that contain these values are kept. \n\n
+    Returns: pd.DataFrame 
+    '''
+    df = df.copy()
+    for key, items in filter_dict.items():
+        df = df[df[key].isin(items)]
+    return df
